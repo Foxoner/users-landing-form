bde511fa6b131ec5b9f2298ac883e929b9b1aaa5
Foxoner <vadim.avercon@gmail.com>
2022-12-27 02:05:52 +0200

    adding webpack build

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Form from './components/Form/Form';
 import Header from './components/Header/Header';
